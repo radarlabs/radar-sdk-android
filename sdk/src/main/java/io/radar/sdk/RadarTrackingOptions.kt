680a45f3b36c93c556803c49647e739c9db3b1fd
Nick Patrick <nicholasjohnpatrick@gmail.com>
2020-01-07 11:23:50 -0500

    update tracking options

--- sdk/src/main/java/io/radar/sdk/RadarTrackingOptions.kt
+++ sdk/src/main/java/io/radar/sdk/RadarTrackingOptions.kt
@@ -94,9 +94,9 @@ data class RadarTrackingOptions(
             replay = RadarTrackingOptionsReplay.REPLAY_OFF,
             sync = RadarTrackingOptionsSync.ALL,
             useStoppedGeofence = false,
-            stoppedGeofenceRadius = 200,
+            stoppedGeofenceRadius = 0,
             useMovingGeofence = false,
-            movingGeofenceRadius = 100
+            movingGeofenceRadius = 0
         )
 
         @JvmField
@@ -136,7 +136,7 @@ data class RadarTrackingOptions(
             useStoppedGeofence = true,
             stoppedGeofenceRadius = 200,
             useMovingGeofence = false,
-            movingGeofenceRadius = 100
+            movingGeofenceRadius = 0
         )
 
         internal const val KEY_DESIRED_STOPPED_UPDATE_INTERVAL = "desiredStoppedUpdateInterval"
