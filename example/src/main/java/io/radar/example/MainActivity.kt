d10ccf52a09211bcabfd5b0835d674296f9ad8dd
Nick Patrick <nicholasjohnpatrick@gmail.com>
2020-05-09 10:33:14 -0400

    sample code

--- example/src/main/java/io/radar/example/MainActivity.kt
+++ example/src/main/java/io/radar/example/MainActivity.kt
@@ -100,7 +100,9 @@ class MainActivity : AppCompatActivity() {
             Log.v("example", "Distance: status = $status; routes.car.distance.value = ${routes?.car?.distance?.value}; routes.car.distance.text = ${routes?.car?.distance?.text}; routes.car.duration.value = ${routes?.car?.duration?.value}; routes.car.duration.text = ${routes?.car?.duration?.text}")
         }
 
-        Radar.mockTracking(origin, destination, Radar.RadarRouteMode.CAR, 10, 2)
+        Radar.mockTracking(origin, destination, Radar.RadarRouteMode.CAR, 10, 2) { status, location, events, user ->
+            Log.v("example", "Mock track: status = ${status}; location = $location; events = $events; user = $user")
+        }
     }
 
 }
