e960b65e7c5d8314afc402bcc5149a399a006de5
Jeff Kao <jeff.kao.000@gmail.com>
2021-06-23 17:16:33 -0400

    Make example app more useable

--- example/src/main/java/io/radar/example/MainActivity.kt
+++ example/src/main/java/io/radar/example/MainActivity.kt
@@ -1,18 +1,23 @@
 package io.radar.example
 
 import android.Manifest
-import android.location.Location
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import io.radar.sdk.Radar
-import io.radar.sdk.RadarTrackingOptions
-import io.radar.sdk.RadarTripOptions
-import java.util.*
+
+enum class RadarAction {
+    GET_LOCATION,
+    TRACK_ONCE
+}
 
 class MainActivity : AppCompatActivity() {
+
+    var currentAction = RadarAction.GET_LOCATION
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +26,17 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val requestCode = 0
             val permissions = arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
             )
             ActivityCompat.requestPermissions(this, permissions, requestCode)
         }
 
+        // initialize Radar
         Radar.initialize(this, "prj_test_pk_0000000000000000000000000000000000000000")
+        Radar.setUserId("my-test-user")
+    }
 
+<<<<<<< HEAD
         Radar.getLocation { status, location, stopped ->
             Log.v("example", "Location: status = ${status}; location = $location; stopped = $stopped")
         }
@@ -127,36 +134,20 @@ class MainActivity : AppCompatActivity() {
                 Radar.completeTrip()
             }
 
-            i++
         }
+    }
 
-        val origin1 = Location("example")
-        origin1.latitude = 40.78382
-        origin1.longitude = -73.97536
-
-        val origin2 = Location("example")
-        origin2.latitude = 40.70390
-        origin2.longitude = -73.98670
-
-        val origins = arrayOf(origin1, origin2)
-
-        val destination1 = Location("example")
-        destination1.latitude = 40.64189
-        destination1.longitude = -73.78779
-
-        val destination2 = Location("example")
-        destination2.latitude = 35.99801
-        destination2.longitude = -78.94294
-
-        val destinations = arrayOf(destination1, destination2)
-
-        Radar.getMatrix(
-            origins,
-            destinations,
-            Radar.RadarRouteMode.CAR,
-            Radar.RadarRouteUnits.IMPERIAL
-        ) { status, matrix ->
-            Log.v("example", "Matrix: status = $status; matrix[0][0].duration.text = ${matrix?.routeBetween(0, 0)?.duration?.text}; matrix[0][1].duration.text = ${matrix?.routeBetween(0, 1)?.duration?.text}; matrix[1][0].duration.text = ${matrix?.routeBetween(1, 0)?.duration?.text};  matrix[1][1].duration.text = ${matrix?.routeBetween(1, 1)?.duration?.text}")
+    fun switchAction(view: View) {
+        val button: Button = findViewById(R.id.action_button)
+        when (currentAction) {
+            RadarAction.GET_LOCATION -> {
+                button.text = "Track once"
+                currentAction = RadarAction.TRACK_ONCE
+            }
+            else -> {
+                button.text = "Get location"
+                currentAction = RadarAction.GET_LOCATION
+            }
         }
     }
 
