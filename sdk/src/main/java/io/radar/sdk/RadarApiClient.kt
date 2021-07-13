2f1b85d15a59b84b212170a4e7e78d59fd9def4f
Jeff Kao <jeff.kao.000@gmail.com>
2021-07-13 18:44:19 -0400

    Publish trackingOptions up to server for POST /track

--- sdk/src/main/java/io/radar/sdk/RadarApiClient.kt
+++ sdk/src/main/java/io/radar/sdk/RadarApiClient.kt
@@ -208,6 +208,7 @@ internal class RadarApiClient(
             params.putOpt("locationAuthorization", RadarUtils.getLocationAuthorization(context))
             params.putOpt("locationAccuracyAuthorization", RadarUtils.getLocationAccuracyAuthorization(context))
             params.putOpt("sessionId", RadarSettings.getSessionId(context))
+            params.putOpt("trackingOptions", RadarSettings.getTrackingOptions(context).toJson())
         } catch (e: JSONException) {
             callback?.onComplete(RadarStatus.ERROR_BAD_REQUEST)
 
