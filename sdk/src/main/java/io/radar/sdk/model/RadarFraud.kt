683f3119dad0d17e0ba92167f9f4c72971202b27
Phil Brown <philip22brown@gmail.com>
2021-12-22 16:47:10 -0600

    Add RadarUser.mocked field (#153)
    Update Radar SDK version to 3.2.4.
    
      * Introduce RadarUser.mocked field and add RadarFraud object for simpler serialization.

---
+++ sdk/src/main/java/io/radar/sdk/model/RadarFraud.kt
@@ -0,0 +1,39 @@
+package io.radar.sdk.model
+
+import org.json.JSONObject
+
+/**
+ * Learned fraud state for the user
+ */
+data class RadarFraud(
+    /**
+     * A boolean indicating whether the user's IP address is a known proxy. May be `false` if Fraud is not enabled.
+     */
+    val proxy: Boolean,
+
+    /**
+     * A boolean indicating whether or not the user's location is being mocked, such as in a simulation. May be
+     * `false` if Fraud is not enabled.
+     */
+    val mocked: Boolean
+) {
+    companion object {
+        private const val PROXY = "proxy"
+        private const val MOCKED = "mocked"
+
+        @JvmStatic
+        fun fromJson(json: JSONObject?): RadarFraud {
+            return RadarFraud(
+                proxy = json?.optBoolean(PROXY, false) ?: false,
+                mocked = json?.optBoolean(MOCKED, false) ?: false
+            )
+        }
+    }
+
+    fun toJson(): JSONObject {
+        return JSONObject().apply {
+            putOpt(PROXY, proxy)
+            putOpt(MOCKED, mocked)
+        }
+    }
+}
