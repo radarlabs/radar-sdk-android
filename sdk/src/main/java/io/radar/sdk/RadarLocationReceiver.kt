261771e89c670082ed969b156b911998ae763c53
Phil Brown <philip22brown@gmail.com>
2022-02-03 21:46:19 -0600

    Remove IMMUTABILITY flag and set radar sdk version to 3.3.3 (#177) (#178)

--- sdk/src/main/java/io/radar/sdk/RadarLocationReceiver.kt
+++ sdk/src/main/java/io/radar/sdk/RadarLocationReceiver.kt
@@ -28,8 +28,8 @@ class RadarLocationReceiver : BroadcastReceiver() {
             val intent = baseIntent(context).apply {
                 action = ACTION_LOCATION
             }
-            val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
             } else {
                 PendingIntent.FLAG_UPDATE_CURRENT
             }
