52e9c59f29bf739bf1dbafa885777f8d54c023ff
phil-brown <philip22brown@gmail.com>
2022-03-02 15:14:10 -0600

    Add unit tests for RadarLocationReceiver.onReceive

---
+++ sdk/src/test/java/io/radar/sdk/RadarLocationReceiverTest.kt
@@ -0,0 +1,29 @@
+package io.radar.sdk
+
+import android.content.Intent
+import android.os.Build
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.P])
+class RadarLocationReceiverTest {
+
+    @Test
+    fun testOnReceive() {
+        assertFalse(Radar.initialized)
+        try {
+            RadarLocationReceiver().onReceive(ApplicationProvider.getApplicationContext(), Intent(""))
+            assertTrue(Radar.initialized)
+        } catch (ignored: UninitializedPropertyAccessException) {
+            fail("onReceive error - Radar not initialized properly")
+        }
+    }
+
+}
