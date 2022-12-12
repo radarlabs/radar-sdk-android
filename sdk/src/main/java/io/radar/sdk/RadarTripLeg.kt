cc7c0c0a6bb3eb28182cd8610c587a2f90fd3537
cory pisano <cory.pisano@gmail.com>
2022-12-12 16:47:43 +0200

    update RadarTripLeg model

--- sdk/src/main/java/io/radar/sdk/RadarTripLeg.kt
+++ sdk/src/main/java/io/radar/sdk/RadarTripLeg.kt
@@ -1,5 +1,6 @@
 package io.radar.sdk
 
+import org.json.JSONArray
 import org.json.JSONObject
 import java.util.*
 
@@ -21,5 +22,46 @@ data class RadarTripLeg(
     // approachingAt
     // scheduledArrivalAt
 ) {
+
+    companion object {
+        internal const val KEY_DESTINATION = "destination"
+        internal const val KEY_METADATA = "metadata"
+        internal const val KEY_SCHEDULED_ARRIVAL_AT = "scheduledArrivalAt"
+
+        // destination
+        internal const val KEY_DESTINATION_GEOFENCE_TAG = "destinationGeofenceTag"
+        internal const val KEY_DESTINATION_GEOFENCE_EXTERNAL_ID = "destinationGeofenceExternalId"
+        internal const val KEY_DESTINATION_GEOFENCE_ID = "destinationGeofenceId"
+        internal const val KEY_DESTINATION_ADDRESS = "address"
+        internal const val KEY_DESTINATION_COORDINATES = "coordinates"
+
+        @JvmStatic
+        fun toJson(legs: Array<RadarTripLeg> ?): JSONArray? {
+            if (legs == null) {
+                return null
+            }
+
+            val arr = JSONArray()
+            legs.forEach { legs ->
+                arr.put(legs.toJson())
+            }
+            return arr
+        }
+    }
+
+    fun toJson(): JSONObject {
+        val obj = JSONObject()
+        val destination = JSONObject()
+        destination.put(RadarTripLeg.KEY_DESTINATION_GEOFENCE_TAG, destinationGeofenceTag)
+        destination.put(RadarTripLeg.KEY_DESTINATION_GEOFENCE_EXTERNAL_ID, destinationGeofenceExternalId)
+        destination.put(RadarTripLeg.KEY_DESTINATION_GEOFENCE_ID, destinationGeofenceId)
+        destination.put(RadarTripLeg.KEY_DESTINATION_ADDRESS, address)
+        destination.put(RadarTripLeg.KEY_DESTINATION_COORDINATES, coordinates)
+
+        obj.put(RadarTripLeg.KEY_METADATA, metadata)
+        obj.put(RadarTripLeg.KEY_DESTINATION, destination)
+//        obj.put(RadarTripLeg.KEY_SCHEDULED_ARRIVAL_AT, scheduledArrivalAt?.time)
+        return obj
+    }
 
 }
