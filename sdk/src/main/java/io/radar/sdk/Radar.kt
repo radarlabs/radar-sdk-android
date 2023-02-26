d204017973013250868978c2445df7916802c70e
jsani-radar <jay@radar.io>
2023-02-26 13:54:33 -0500

    dont fire event listeners for custom events (#263)

--- sdk/src/main/java/io/radar/sdk/Radar.kt
+++ sdk/src/main/java/io/radar/sdk/Radar.kt
@@ -2603,12 +2603,6 @@ object Radar {
                         return
                     }
 
-                    if (events != null) {
-                        // The events are returned in the completion handler, but they're also
-                        // sent back via the RadarReceiver.
-                        receiver?.onEventsReceived(context, events, user)
-                    }
-
                     handler.post {
                         callback.onComplete(status, location, events, user)
                     }
