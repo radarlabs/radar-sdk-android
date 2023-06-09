778866e894cc534bde5872b68ba190d860ff7e32
Liam Meier <liam.meier@radar.com>
2023-06-09 14:37:26 -0400

    Revert stoppedUpdateInterval change

--- sdk/src/main/java/io/radar/sdk/RadarTrackingOptions.kt
+++ sdk/src/main/java/io/radar/sdk/RadarTrackingOptions.kt
@@ -372,8 +372,8 @@ data class RadarTrackingOptions(
          */
         @JvmField
         val RESPONSIVE = RadarTrackingOptions(
-            desiredStoppedUpdateInterval = 3600,
-            fastestStoppedUpdateInterval = 1200,
+            desiredStoppedUpdateInterval = 0,
+            fastestStoppedUpdateInterval = 0,
             desiredMovingUpdateInterval = 150,
             fastestMovingUpdateInterval = 30,
             desiredSyncInterval = 20,
