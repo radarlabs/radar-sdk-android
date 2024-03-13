05c62ed27077661b836bf0a3c7735ea4a745e318
Nick Patrick <nicholasjohnpatrick@gmail.com>
2024-03-13 11:19:33 -0400

    fix lifecycle callbacks

--- sdk/src/main/java/io/radar/sdk/RadarActivityLifecycleCallbacks.kt
+++ sdk/src/main/java/io/radar/sdk/RadarActivityLifecycleCallbacks.kt
@@ -50,7 +50,7 @@ internal class RadarActivityLifecycleCallbacks(
                 if (updated) {
                     val usage = "resume"
                     Radar.apiClient.getConfig(usage, false, object : RadarApiClient.RadarGetConfigApiCallback {
-                        override fun onComplete(config: RadarConfig) {
+                        override fun onComplete(status: Radar.RadarStatus, config: RadarConfig) {
                             Radar.locationManager.updateTrackingFromMeta(config.meta)
                             RadarSettings.setFeatureSettings(activity.applicationContext, config.meta.featureSettings)
                         }
