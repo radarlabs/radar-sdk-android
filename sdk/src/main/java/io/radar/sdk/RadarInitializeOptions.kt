7b3553fabf12624250a0ac81c6be3726ef5d81a2
ShiCheng Lu <shicheng.lu@radar.com>
2024-06-13 09:26:47 -0400

    add all initialize options to RadarInitializeOptions

--- sdk/src/main/java/io/radar/sdk/RadarInitializeOptions.kt
+++ sdk/src/main/java/io/radar/sdk/RadarInitializeOptions.kt
@@ -1,38 +1,31 @@
 package io.radar.sdk
 
 import org.json.JSONObject
-import java.util.Date
+import io.radar.sdk.Radar.RadarLocationServicesProvider
 
 data class RadarInitializeOptions(
+    /**
+     * A boolean indicating whether to enable additional fraud detection signals for location verification.
+     */
     var fraud: Boolean = false,
+
+    /**
+     * An optional stable unique ID for the user to set upon initialization. No-op if null.
+     */
     var userId: String? = null,
+
+    /**
+     * An optional set of custom key-value pairs for the user. Must have 16 or fewer keys and values of type string, boolean, or number. No-op if null. 
+     */
     var metadata: JSONObject? = null,
-) {
-    companion object {
 
-        internal const val KEY_FRAUD = "fraud"
-        internal const val KEY_USER_ID = "userId"
-        internal const val KEY_METADATA = "metadata"
+    /**
+     * An optional receiver for the client-side delivery of events.
+     */
+    var receiver: RadarReceiver? = null,
 
-
-
-        @JvmStatic
-        fun fromJson(obj: JSONObject): RadarInitializeOptions {
-            val fraud = if (obj.isNull(KEY_FRAUD)) null else obj.optBoolean(KEY_FRAUD)
-            val userId = if (obj.isNull(KEY_USER_ID)) null else obj.optString(KEY_USER_ID)
-            val metadata = if (obj.isNull(KEY_METADATA)) null else obj.optJson(KEY_METADATA)
-
-            return RadarInitializeOptions(fraud, userId, metadata)
-        }
-    }
-
-    fun toJson(): JSONObject {
-        val obj = JSONObject()
-
-        obj.put(KEY_FRAUD, fraud)
-        obj.put(KEY_USER_ID, userId)
-        obj.put(KEY_METADATA, metadata)
-
-        return obj
-    }
-}
+    /**
+     * The location services provider.
+     */
+    var provider: RadarLocationServicesProvider = RadarLocationServicesProvider.GOOGLE,
+)
