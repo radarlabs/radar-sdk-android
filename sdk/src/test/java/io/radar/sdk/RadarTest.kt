f2582d9fb5058983fb7822a08ea07e8983283dc4
ShiCheng Lu <shicheng.lu@radar.com>
2024-07-10 17:05:50 -0400

    change to trackOnceOnAppOpen in tests

--- sdk/src/test/java/io/radar/sdk/RadarTest.kt
+++ sdk/src/test/java/io/radar/sdk/RadarTest.kt
@@ -1559,7 +1559,7 @@ class RadarTest {
 
     @Test
     fun test_Radar_setSdkConfiguration() {
-        val sdkConfiguration = RadarSdkConfiguration(Radar.RadarLogLevel.WARNING, true, false, true)
+        val sdkConfiguration = RadarSdkConfiguration(Radar.RadarLogLevel.WARNING, true, true)
 
         RadarSettings.setUserDebug(context, false)
         RadarSettings.setSdkConfiguration(context, sdkConfiguration)
@@ -1592,7 +1592,6 @@ class RadarTest {
         val savedSdkConfiguration = RadarSettings.getSdkConfiguration(context)
         assertEquals(Radar.RadarLogLevel.INFO, savedSdkConfiguration?.logLevel)
         assertEquals(true, savedSdkConfiguration?.startTrackingOnInitialize)
-        assertEquals(true, savedSdkConfiguration?.trackOnceOnInitialize)
-        assertEquals(true, savedSdkConfiguration?.trackOnceOnResume)
+        assertEquals(true, savedSdkConfiguration?.trackOnceOnAppOpen)
     }
 }
