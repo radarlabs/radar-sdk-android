5d292b1d9e9bccf407e2a128c5e298507e94b414
Nick Patrick <nicholasjohnpatrick@gmail.com>
2024-08-17 07:40:22 -0400

    logs

--- sdk/src/main/java/io/radar/sdk/RadarVerificationManager.kt
+++ sdk/src/main/java/io/radar/sdk/RadarVerificationManager.kt
@@ -413,7 +413,7 @@ internal class RadarVerificationManager(
         if (!isIntegrityApiIncluded()) {
             val integrityException = "Integrity API not included"
 
-            logger.w("Integrity API not included")
+            logger.w(integrityException)
 
             block(null, integrityException)
 
@@ -456,7 +456,7 @@ internal class RadarVerificationManager(
         if (!isIntegrityApiIncluded()) {
             val integrityException = "Integrity API not included"
 
-            logger.w("Integrity API not included")
+            logger.w(integrityException)
 
             block(null, integrityException)
 
