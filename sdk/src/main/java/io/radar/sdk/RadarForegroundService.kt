949960caea35b9e9dc6104f202a0515c2bf58ba8
Kenny Hu <kenny.hu@radar.com>
2025-08-06 10:56:23 -0400

    .

--- sdk/src/main/java/io/radar/sdk/RadarForegroundService.kt
+++ sdk/src/main/java/io/radar/sdk/RadarForegroundService.kt
@@ -100,7 +100,6 @@ class RadarForegroundService : Service() {
         if (iconColor.isNotEmpty()) {
             builder.setColor(Color.parseColor(iconColor))
         }
-
         try {	
             val intent: Intent	
             val deepLinkString = extras?.getString("deepLink")	
@@ -111,21 +110,21 @@ class RadarForegroundService : Service() {
                 intent.addCategory(Intent.CATEGORY_BROWSABLE)
             } else {
                 // If no deep link, open the main app
-                val packageManager = applicationContext.packageManager	
+                val packageManager = applicationContext.packageManager
                 intent = packageManager.getLaunchIntentForPackage(applicationContext.packageName) ?: 
                     Intent(applicationContext, Class.forName(extras?.getString("activity")))
             }	
 
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK	
-            val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {	
-                PendingIntent.FLAG_IMMUTABLE	
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                PendingIntent.FLAG_IMMUTABLE
             } else {
                 0
             }
             val pendingIntent = PendingIntent.getActivity(this, 0, intent, flags)
             builder = builder.setContentIntent(pendingIntent)
         } catch (e: Exception) {
-            logger.e("Error setting foreground service content intent", RadarLogType.SDK_EXCEPTION, e)	
+            logger.e("Error setting foreground service content intent", RadarLogType.SDK_EXCEPTION, e)
         }
         val notification = builder.build()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
