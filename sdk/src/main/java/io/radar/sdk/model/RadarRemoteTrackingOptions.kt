c3b7ef30b650f1f6341f627999db2a8d8ea2230a
Alan <alan.charles@radar.com>
2026-04-15 16:29:37 -0600

    Add RadarRemoteTrackingOptions

---
+++ sdk/src/main/java/io/radar/sdk/model/RadarRemoteTrackingOptions.kt
@@ -0,0 +1,59 @@
+package io.radar.sdk.model
+
+import androidx.core.graphics.component1
+import androidx.core.graphics.component2
+import androidx.core.graphics.component3
+import androidx.core.graphics.component4
+import io.radar.sdk.RadarTrackingOptions
+import org.json.JSONArray
+import org.json.JSONObject
+
+internal data class RadarRemoteTrackingOptions(
+    val type: String,
+    val trackingOptions: RadarTrackingOptions,
+    val geofenceTags: List<String>?
+) {
+    companion object {
+        fun fromJson(json: JSONObject): RadarRemoteTrackingOptions? {
+            val type = json.optString("type") ?: return null
+            val trackingOptionsJson = json.optJSONObject("trackingOptions") ?: return  null
+            val trackingOptions = RadarTrackingOptions.fromJson(trackingOptionsJson)
+
+            val geofenceTags = json.optJSONObject("geofenceTags")?.let { arr ->
+                (0 until arr.length()).mapNotNull { arr.optString(it) }
+            }
+
+            return RadarRemoteTrackingOptions(type, trackingOptions, geofenceTags)
+        }
+
+        fun fromJsonArray(array: JSONArray?): List<RadarRemoteTrackingOptions>? {
+            if (array == null || array.length() == 0) return null
+            return (0 until array.length()).mapNotNull { i ->
+                array.optJSONObject(i)?.let { fromJson(it) }
+            }.ifEmpty { null }
+        }
+
+        fun toJsonArray(options: List<RadarRemoteTrackingOptions>?): JSONArray? {
+            if (options == null) return null
+            return JSONArray().apply {
+                options.forEach { put(it.toJson()) }
+            }
+        }
+
+        fun trackingOptions(forKey: String, options: List<RadarRemoteTrackingOptions>?): RadarTrackingOptions? {
+            return options?.firstOrNull { it.type == forKey }?.trackingOptions
+        }
+
+        fun geofenceTags(forKey: String, options: List<RadarRemoteTrackingOptions>?): List<String>? {
+            return options?.firstOrNull { it.type == forKey }?.geofenceTags
+        }
+    }
+
+    fun toJson(): JSONObject {
+        return JSONObject().apply {
+            put("type", type)
+            put("trackingOptions", trackingOptions.toJson())
+            geofenceTags?.let  { put("geofenceTags", JSONArray(it)) }
+        }
+    }
+}
